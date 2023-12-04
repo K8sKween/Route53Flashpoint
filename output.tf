7365ec3b9eef4a6f6fcdac170ffbce802275aa94
Victoria Mann <victoria.mann@otus.com>
2023-12-04 15:46:29 -0600

    add api_id to output

--- output.tf
+++ output.tf
@@ -7,3 +7,8 @@ output "reports_endpoint" {
   description = "API Gateway endpoint for reports resource"
   value       = aws_api_gateway_resource.reports_resource.id
 }
+
+output "api_id" {
+  description = "API Gateway ID"
+  value       = aws_api_gateway_rest_api.route53_api.id
+}
